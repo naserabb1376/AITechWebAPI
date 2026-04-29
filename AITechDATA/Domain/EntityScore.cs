bda175caf06937496f0bbd5baa5c2fb3cc5b0930
Mohammad Tavoosi <tavoosi.insta20@gmail.com>
2026-04-30 02:06:12 +0330

    1111

--- AITechDATA/Domain/EntityScore.cs
+++ AITechDATA/Domain/EntityScore.cs
@@ -9,21 +9,21 @@ namespace AITechDATA.Domain
 {
     public class EntityScore : BaseEntity
     {
-        public string? ScoreItemTitle { get; set; }
-        public float ScoreItemRawScore { get; set; } = 0.0f;
-        public float ScoreItemWeightPercent { get; set; } = 0.0f;
-        public float ScoreItemWeightedScore { get; set; } = 0.0f;
-        public float ScoreItemTotalScore { get; set; } = 0.0f;
-        public long? ScoreItemParentId { get; set; }
-        public string ScoreItemKey { get; set; }
-        public int RecordLevel { get; set; } // 0: Parent 1: Child 2: Data
+        public string? ScoreItemTitle { get; set; }  // عنوان سر شاخص یا زیر شاخص
+        public float ScoreItemRawScore { get; set; } = 0.0f;  // امتیاز خام داده های شاخص (اجباری برای سطح داده 2)
+        public float ScoreItemWeightPercent { get; set; } = 0.0f; //وزن درصدی هر شاخص اجباری برای سطح داده 2
+        public float ScoreItemWeightedScore { get; set; } = 0.0f; // امتیاز هر زیر شاخص با احتساب وزن برای لایه داده 2  ScoreItemRawScore / 100) * ScoreItemWeightPercent
+        public float ScoreItemTotalScore { get; set; } = 0.0f; // امتیاز کل با احتساب همه زیر شاخص برای هر گزارش مدیریتی یا وظیفه
+        public long? ScoreItemParentId { get; set; } // کد والد هر رکورد سرشاخص -> زیر شاخص -> داده
+        public string ScoreItemKey { get; set; } //  کلید انگلیسی مربوط به هر شاخص یا زیر شاخص اجباری برای همه سطح ها و یونیک
+        public int RecordLevel { get; set; } // 0: Parent 1: Child 2: Data وجه تمایز سطح هر رکورد
 
         public long UserId { get; set; } // کلید خارجی به User
         public User User { get; set; } // ارتباط با User 
 
         public string EntityType { get; set; } // نام جدول مبدا (مثلاً "User", "Course", "Event")
         public long ForeignKeyId { get; set; } // کلید خارجی به رکورد اصلی
-        public string? TargetObjName { get; set; }
+        public string? TargetObjName { get; set; } // نام یا عنوان یا هر نشانی از رکورد مورد نظر وظیفه یا گزارش مدیریتی (اختیاری)
         public string? Description { get; set; } = ""; // توضیحات
         public long CreatorId { get; set; } = 0; // کاربر ایجاد کننده
 
